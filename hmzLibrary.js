9f9722e3561cd46ff8be88caceb597521ec2bcc0
Ma Seo Sen | Peb Yog Hmong <pyhteam.solutions@gmail.com>
2023-06-22 13:37:54 +0700

    add

--- hmzLibrary.js
+++ hmzLibrary.js
@@ -19,7 +19,7 @@
      * @example
      * 
      **/
-    function createTable(data, colums, configs) {
+    function createTable(data, colums, configs, pageginateConfig = null) {
         var config = {
             id: configs.id || "hmzDataTable",
             class: configs.class || "table table-striped table-bordered table-hover",
@@ -28,14 +28,32 @@
                 delete: configs.showAction.delete || false,
                 view: configs.showAction.view || false
             },
-            buttons: configs.buttons || []
+            buttons: configs.buttons || [],
+            paginate: configs.paginate || false
         }
-        let html = document.getElementById("hmzTable");
-        let table = document.createElement("table");
-        table.setAttribute("class", config.class);
-        table.setAttribute("id", config.id);
-        let thead = document.createElement("thead");
+
+        /**
+         * @param {object} pageginateConfig
+         * @param {number} pageginateConfig.pageIndex
+         * @param {number} pageginateConfig.pageSize
+         * @param {number} pageginateConfig.totalItems
+         * @param {function} pageginateConfig.eventPage
+         * @return {object} pageConfig
+         * @description generate pageConfig
+         * @example
+         * 
+         */
+        var pageConfig = {
+            pageIndex: pageginateConfig.pageIndex || 1,
+            pageSize: pageginateConfig.pageSize || 10,
+            totalItems: pageginateConfig.totalItems || 0,
+            event: pageginateConfig.event || null,
+            apiUrl: pageginateConfig.apiUrl || null,
+            method: pageginateConfig.method || "GET",
+        }
+
         // check showAction
+       
         if (config.showAction) {
             colums = [
                 ...colums,
@@ -47,6 +65,11 @@
                 }
             ]
         }
+        let html = document.getElementById("hmzTable");
+        let table = document.createElement("table");
+        table.setAttribute("class", config.class);
+        table.setAttribute("id", config.id);
+        let thead = document.createElement("thead");
         // buttons 
         // Example data buttons
         // const buttonsDefault = [
@@ -81,15 +104,44 @@
         });
         table.appendChild(thead);
         // generate tbody by data
+        let hmzTbody = createTBodyTable(data, colums, config);
+        table.appendChild(hmzTbody);
+
+        html.appendChild(table);
+        // check paginate
+        if (config.paginate == true && pageConfig) {
+            let paginate = createPagination(pageConfig)
+            html.appendChild(paginate);
+        }
+        return html;
+    }
+
+
+    function createTBodyTable(data, colums, config) {
+        // check exist colums action
+        if(config.showAction){
+            if(colums.filter(x => x.field == "action").length == 0){
+                colums = [
+                    ...colums,
+                    {
+                        id: colums.length + 1,
+                        field: "action",
+                        name: "action",
+                        type: "html"
+                    }
+                ]
+            }
+        }
         let tbody = document.createElement("tbody");
-        data.forEach(element => {
+        tbody.setAttribute("id", "hmzTableBody");
+        data.forEach(item => {
             let tr = document.createElement("tr");
             colums.forEach(colum => {
                 // check property in data has in colums
-                if (colum.field in element) {
+                if (colum.field in item) {
 
                     let td = document.createElement("td");
-                    td.innerHTML = element[colum.field];
+                    td.innerHTML = item[colum.field];
                     tr.appendChild(td);
                 }
                 // check showAction
@@ -97,29 +149,28 @@
                     // match to colums field = action
                     if (colum.field == "action") {
                         // generate buttons > 0
-                        let btns = createButtonsTable(config.buttons, element.id);
+                        let btns = createButtonsTable(config.buttons, item.id);
                         let td = document.createElement("td");
                         td.appendChild(btns);
                         tr.appendChild(td);
                     }
+                    
                 }
+                
 
             });
             tbody.appendChild(tr);
         });
-        table.appendChild(tbody);
-
-        html.appendChild(table);
-        return html;
+        return tbody;
     }
-    function createButtonsTable(buttons,id) {
+    function createButtonsTable(buttons, id) {
         if (buttons.length > 0) {
             let btns = document.createElement("div");
-            btns.setAttribute("style", "display: flex; justify-content: space-between;");
             buttons.forEach(button => {
                 let btn = document.createElement("button");
-                btn.setAttribute("id", button.id+"_"+ id);
+                btn.setAttribute("id", button.id + "_" + id);
                 btn.setAttribute("class", button.class);
+                btn.setAttribute("style", "margin-left: 5px;");
                 btn.setAttribute("type", "button");
                 btn.setAttribute("data-id", id);
                 btn.innerHTML = `<i class="${button.icon}"></i> ${button.text}`;
@@ -127,9 +178,162 @@
                 btns.appendChild(btn);
             });
             return btns
-            
         }
     }
+
+    // My object
+    // let pageConfig = {
+    //     pageIndex: 1,
+    //     pageSize: 10,
+    //     totalItems: 0,
+    //     event: function () {
+
+    //     }
+    // }
+    function createPagination(config) {
+        // Create the nav element
+        var nav = document.createElement("nav");
+        nav.setAttribute("aria-label", "Page navigation example");
+
+        // Create the pagination ul element
+        var pagination = document.createElement("ul");
+        pagination.setAttribute("class", "pagination pagination-sm");
+
+        // Calculate the total number of pages
+        var totalPages = Math.ceil(config.totalItems / config.pageSize);
+
+        // Check if it's the first page
+        var isFirstPage = config.pageIndex === 1;
+
+        // Check if it's the last page
+        var isLastPage = config.pageIndex === totalPages;
+
+        // Create the previous button
+        var liPrevious = document.createElement("li");
+        liPrevious.setAttribute("class", "page-item");
+        if (isFirstPage) {
+            liPrevious.classList.add("disabled");
+        }
+        var aPrevious = document.createElement("a");
+        aPrevious.setAttribute("class", "page-link");
+        aPrevious.setAttribute("id", "previous");
+        aPrevious.setAttribute("href", "#");
+        aPrevious.setAttribute("aria-label", "Previous");
+        var spanPrevious = document.createElement("span");
+        spanPrevious.setAttribute("aria-hidden", "true");
+        spanPrevious.textContent = "Previous";
+        aPrevious.appendChild(spanPrevious);
+        liPrevious.appendChild(aPrevious);
+        pagination.appendChild(liPrevious);
+
+        // Create the page buttons
+        for (var i = 1; i <= totalPages; i++) {
+            var liPage = document.createElement("li");
+            liPage.setAttribute("class", "page-item");
+            liPage.setAttribute("id", ""+i);
+            if (i === config.pageIndex) {
+                liPage.classList.add("active");
+            }
+            var aPage = document.createElement("a");
+            aPage.setAttribute("class", "page-link");
+            aPage.setAttribute("href", "#");
+            aPage.setAttribute("id", ""+i);
+            aPage.textContent = i;
+            liPage.appendChild(aPage);
+            pagination.appendChild(liPage);
+        }
+
+        // Create the next button
+        var liNext = document.createElement("li");
+        liNext.setAttribute("class", "page-item");
+        if (isLastPage) {
+            liNext.classList.add("disabled");
+        }
+        var aNext = document.createElement("a");
+        aNext.setAttribute("class", "page-link");
+        aNext.setAttribute("href", "#");
+        aNext.setAttribute("aria-label", "Next");
+        aNext.setAttribute("id", "next");
+        var spanNext = document.createElement("span");
+        spanNext.setAttribute("aria-hidden", "true");
+        spanNext.textContent = "Next";
+        aNext.appendChild(spanNext);
+        liNext.appendChild(aNext);
+        pagination.appendChild(liNext);
+
+        nav.appendChild(pagination);
+
+        // Add click event listeners to the page buttons
+        var pageLinks = pagination.querySelectorAll(".page-link");
+
+        pageLinks.forEach(function (pageLink) {
+            pageLink.addEventListener("click", function (event) {
+                event.preventDefault();
+
+                // Get the id of the clicked page link
+                var id = this.id;
+
+                // Check if the clicked page is the previous or next button
+                if (id === "previous") {
+                    // Handle previous page
+                    var currentPage = pagination.querySelector(".page-item.active");
+                    var prevPage = currentPage.previousElementSibling;
+
+                    if (prevPage && !prevPage.classList.contains("disabled")) {
+                        var pageIndex = parseInt(prevPage.id);
+                        config.event(pageIndex);
+
+                        // Update UI and activate the new page
+                        currentPage.classList.remove("active");
+                        prevPage.classList.add("active");
+
+                        // Disable or enable Previous and Next buttons
+                        liPrevious.classList.toggle("disabled", pageIndex === 1);
+                        liNext.classList.remove("disabled");
+                    }
+                } else if (id === "next") {
+                    // Handle next page
+                    var currentPage = pagination.querySelector(".page-item.active");
+                    var nextPage = currentPage.nextElementSibling;
+
+                    if (nextPage && !nextPage.classList.contains("disabled")) {
+                        var pageIndex = parseInt(nextPage.id);
+                        config.event(pageIndex);
+
+                        // Update UI and activate the new page
+                        currentPage.classList.remove("active");
+                        nextPage.classList.add("active");
+
+                        // Disable or enable Previous and Next buttons
+                        liPrevious.classList.remove("disabled");
+                        liNext.classList.toggle("disabled", pageIndex === totalPages);
+                    }
+                } else {
+                    
+                    // Handle regular page click
+                    var clickedPageIndex = parseInt(this.id);
+                    
+
+                    if (!this.parentNode.classList.contains("active")) {
+                        config.event(clickedPageIndex);
+
+                        // Update UI and activate the new page
+                        var activePageItem = pagination.querySelector(".page-item.active");
+                        activePageItem.classList.remove("active");
+                        this.parentNode.classList.add("active");
+
+                        // Disable or enable Previous and Next buttons
+                        liPrevious.classList.toggle("disabled", clickedPageIndex === 1);
+                        liNext.classList.toggle("disabled", clickedPageIndex === totalPages);
+                    }
+                }
+            });
+        });
+        return nav;
+    }
+
+
+
     // Example POST method implementation:
     /* let data = HMZ.httpClient(API_URL, "GET")
         .then(function (res) {
@@ -154,7 +358,9 @@
     //#region   public methods
     var HMZ = {
         httpClient: hmzHttpClient,
-        createTable: createTable
+        createTable: createTable,
+        createTBodyTable: createTBodyTable,
+        
     };
     global.HMZ = HMZ;
     //#endregion
